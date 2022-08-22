ff4d7c69bcbcb2815ccf4b3c21de613bbd2c437d
Robinson <102079993+RobinsonNakama@users.noreply.github.com>
2022-08-22 17:51:11 -0300

    ver 22/08

---
+++ scritps/java.js
@@ -0,0 +1,12 @@
+
+alert('Obrigado por olhar o meu curriculo')
+
+
+function text() {
+    let text = document.getElementById("icon")
+    text.addEventListener(onmouseenter,tecnologia)
+}
+function reset() {
+   // document.getElementById("icon").innerHTML = ""
+}
+
